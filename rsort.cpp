92920a41a784703589e59efe8173d7480b19dfd5
Shivanker Goel <shivanker.goel@gmail.com>
2015-02-11 00:22:11 +0530

    Some final improvements in radix sort

--- rsort.cpp
+++ rsort.cpp
@@ -23,14 +23,12 @@
 
 #define PRAGMA(x) _Pragma(#x)
 
-const int ser_n = 1<<12;
+const int ser_n = 1<<9;
 int num_procs, for_ser_n;
 
-void psum(int *data, int n) {
+void psum(int *data, int n, int *data2) {
   if(n < 2)
     return;
-
-  int *data2 = new int[n>>1];
 
   int i;
   taskfor(i, n/2, data2, data)
@@ -38,7 +36,7 @@ void psum(int *data, int n) {
     data2[i] = data[i<<1] + data[(i<<1) + 1];
   } endtaskfor;
 
-  psum(data2, n>>1);
+  psum(data2, n>>1, data2+(n>>1));
 
   taskfor(i, n/2, data, data2)
   {
@@ -48,24 +46,21 @@ void psum(int *data, int n) {
 
   if(n&1)
     data[n-1] += data2[(n>>1)-1];
-
-  delete [] data2;
 }
 
-void rSort_helper(dataType *data, int n, int shift, dataType *buffer)    {
+void rSort_helper(dataType *data, int n, int shift, dataType *buffer, int *bucket, int *buffer2)    {
     // printf ("%d\n", shift);
-    if(n <= 1 || shift < 0) {
+    if(n <= 1 || shift < 0)
       return;
-    }
 
     int i, invert = (shift == 63) ? 1 : 0;
-    int *bucket = new int[n];
 
-    taskfor(i, n, bucket, data, shift, invert)  {
+    taskfor(i, n)  {
       bucket[i] = (((long long)data[i].key >> shift) &1) ^ invert;
     } endtaskfor;
 
-    psum(bucket, n);
+    psum(bucket, n, buffer2);
+
     int offset = n-bucket[n-1]-1; // no of zeros - 1
     taskfor(i, n, shift, invert, data, buffer, offset, bucket)   {
       if((((long long)data[i].key >> shift) &1) ^ invert)
@@ -74,34 +69,37 @@ void rSort_helper(dataType *data, int n, int shift, dataType *buffer)    {
         buffer[i-bucket[i]] = data[i];
     } endtaskfor;
 
-    delete [] bucket;
-
-    taskfor(i, n, data, buffer)
+    taskfor(i, n)
     {
       data[i] = buffer[i];
     } endtaskfor;
 
     i = ++offset; // i = no of zeros
-    #pragma omp task if(i > ser_n) firstprivate(data)
-    rSort_helper(data, i, shift-1, buffer);
+    #pragma omp task if(i > ser_n)
+    rSort_helper(data, i, shift-1, buffer, bucket, buffer2);
 
-    #pragma omp task if(n-i > ser_n) firstprivate(data, i, n)
-    rSort_helper(data+i, n-i, shift-1, buffer+i);
+    #pragma omp task if(n-i > ser_n)
+    rSort_helper(data+i, n-i, shift-1, buffer+i, bucket+i, buffer2+i);
 }
 
 void rSort(dataType *data, int n)   {
-    omp_set_nested(1);  // for psum
     num_procs = omp_get_num_procs();
-    for_ser_n = ((100)*num_procs);
+    for_ser_n = ((1<<9)*num_procs);
+
+    // instead of allocating and deallocating buffers all the time, just allocate once and share
     dataType *newdata = new dataType[n];
+    int *bucket = new int[n];
+    int *buf2 = new int [n];
     #pragma omp parallel firstprivate(data, n)
     {
         #pragma omp master
         {
             // printf("%d threads\n", omp_get_num_threads());
-            rSort_helper(data, n, 63, newdata);
+            rSort_helper(data, n, 63, newdata, bucket, buf2);
         }
     }
 
     delete [] newdata;
+    delete [] bucket;
+    delete [] buf2;
 }
